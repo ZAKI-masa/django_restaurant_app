edcd60addeb68afcbc09045200a5c524ae012008
ZAKI-masa <mpmpm19868@gmail.com>
2025-11-11 18:14:52 +0900

    first commit

---
+++ gourmet_guide/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    
+
+    author = models.ForeignKey(
+
+        'auth.User',  # Djangoのデフォルトのユーザーモデルを指定
+
+        on_delete=models.CASCADE,  # ユーザーが削除された場合、関連するカテゴリも削除
+
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+
+        return self.name
+        
+
+
+
+class Shop(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    address = models.CharField(max_length=255)
+
+    
+
+    author = models.ForeignKey(
+
+        'auth.User',  
+
+        on_delete=models.CASCADE, 
+
+    )
+
+    
+
+    category = models.ForeignKey(
+
+        Category,  
+
+        on_delete=models.PROTECT,  
+
+    )
+
+    
+
+    created_at = models.DateTimeField(auto_now_add=True)    
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+         # 飲食店の名前を文字列として返す（管理画面などで表示される）
+        return self.name
+        
+
+    def get_absolute_url(self):
+        # 新規作成したら詳細ページにリダイレクト
+        return reverse('gourmet_guide:detail', kwargs={'pk': self.pk})
+
+
---
+++ gourmet_guide/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import generic
+#genericとは
+
+from .models import Category, Shop
+from django.urls import reverse_lazy
+import requests
+import urllib
+
+
+class IndexView(generic.ListView):
+#Listview=一覧表示でmodel=Shopで指定
+    model = Shop
+
+
+class DetailView(generic.DetailView):
+    template_name='gourmet_guide/wonderful_shop_detail.html'
+
+    model=Shop
+    # #modelを指定  shopと関係のないデータを渡したい時
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        shop_instance=self.get_object()
+        print("shop_instance:",shop_instance)
+        address=shop_instance.address
+        make_Url="https://msearch.gsi.go.jp/address-search/AddressSearch?q="
+
+        # 住所をURLエンコード（日本語や特殊文字をURL安全な形式に変換）
+        # 例: "東京都" → "%E6%9D%B1%E4%BA%AC%E9%83%BD"
+
+        s_quote=urllib.parse.quote(address)
+
+        # 国土地理院APIにリクエストを送信
+        # レスポンスはJSON形式で座標情報などを含む
+
+        response=requests.get(make_Url+s_quote)
+        print("response:",response)
+        print("response_text:",response.text)
+            # response.json()[0] = 最初の検索結果
+
+          # ['geometry']['coordinates'] = [経度, 緯度] の配列
+
+        coordinates = response.json()[0]["geometry"]["coordinates"]
+
+        print("coordinates", coordinates)
+        reversed_coordinates=reversed(coordinates)
+        context["coordinates"]=",".join(map(str,reversed_coordinates))
+        print("context['coordinates']", context['coordinates'])
+
+
+
+        return context
+
+
+class CreateView(generic.edit.CreateView):
+    model=Shop
+    fields = "__all__"#ショップが持ってる登録できる情報全て
+
+class UpdateView(generic.edit.UpdateView):
+    model=Shop
+    fields = "__all__"
+
+class DeleteView(generic.DeleteView):
+    model=Shop
+    # reverse_lazyの引数に表示したいページのnameを指定
+    success_url=reverse_lazy("gourmet_guide:index")
